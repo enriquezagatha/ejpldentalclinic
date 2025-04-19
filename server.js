9d8eaa6a0686ee62be8bb85b90c345b98fa4d755
KreianLanaria <lkd0004@dlsud.edu.ph>
2025-04-20 00:40:06 +0800

    reverted changes of dockerfile

--- server.js
+++ server.js
@@ -142,6 +142,7 @@ app.get("/api/admin/protected-route", authRoutes.authorizeAdmin, (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on http://0.0.0.0:${PORT}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
