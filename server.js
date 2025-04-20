cbdf72fc47ff74944234b52615539b11196e7aab
KreianLanaria <lkd0004@dlsud.edu.ph>
2025-04-20 10:35:48 +0800

    updated server.js for hosting

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ app.get("/api/admin/protected-route", authRoutes.authorizeAdmin, (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
