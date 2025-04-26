dc68b19329c872cfba8fd2832052d5f96c28ac14
AgathaEnriquez <eac0277@dlsud.edu.ph>
2025-04-27 00:37:13 +0800

    staff management and db

--- config/db.js
+++ config/db.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 // MongoDB connection logic
 module.exports.connect = () => {
-  mongoose.connect('mongodb+srv://agathaenriquez:agatha@cluster1.nbelx.mongodb.net/EJPLDentalClinicDB')
+  mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB successfully!'))
   .catch(err => console.error('MongoDB connection error:', err));
 
   //MongoDB Event Listeners
   mongoose.connection.on('disconnected', () => {
     console.log('⚠️ MongoDB disconnected. Reconnecting...');
-    mongoose.connect('mongodb+srv://agathaenriquez:agatha@cluster1.nbelx.mongodb.net/EJPLDentalClinicDB');
+    mongoose.connect(process.env.MONGODB_URI);
   });
 
   mongoose.connection.on('error', (err) => {
